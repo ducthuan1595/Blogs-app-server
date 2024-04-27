5f6fc7b3ee8c2988c01318af08aea32702cc933b
thuantruong <hi.tim.gi.the@gmail.com>
2024-04-27 16:21:20 +0700

    delete file dist

--- dist/middleware/redis/redisType.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.GET_ALL_CATEGORY = exports.GET_ALL_POST = void 0;
-exports.GET_ALL_POST = 'get-all-post';
-exports.GET_ALL_CATEGORY = 'get-all-category';
